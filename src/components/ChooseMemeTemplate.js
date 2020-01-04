b5d0b269a523a07988f53d6f4abd22391f4b7b74
Marek <michalskimarek89@gmail.com>
2020-01-04 16:34:33 +0100

    add responsive selected image

--- src/components/ChooseMemeTemplate.js
+++ src/components/ChooseMemeTemplate.js
@@ -1,47 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { GridList, Container, Typography } from "@material-ui/core";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
+import {
+  GridList,
+  Container,
+  Typography,
+  useMediaQuery,
+  GridListTile,
+  GridListTileBar
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    marginTop: "80px",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper,
-    border: "1px solid rgba(0, 0, 0, 0.12)",
-    padding: "10px"
-  },
-  gridList: {
-    flexWrap: "nowrap",
-    transform: "translateZ(0)"
-  },
-  titleBar: {
-    background:
-      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
-  },
-  tile: {
-    cursor: "pointer"
-  },
-  selectedImage: {
-    marginBottom: "30px",
-    maxHeight: "300px",
-    border: "1px solid rgba(0, 0, 0, 0.12)"
-  },
-  selected: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "10px"
-  }
-}));
 
 const ChooseMemeTemplate = () => {
   const [templates, setTemplates] = useState([]);
   const [selectedTemplate, setSelectedTemplate] = useState([]);
+  const mediaQueryMax320 = useMediaQuery("(max-width:320px)");
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: "flex",
+      marginTop: "80px",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+      border: "1px solid rgba(0, 0, 0, 0.12)",
+      padding: "10px"
+    },
+    gridList: {
+      flexWrap: "nowrap",
+      transform: "translateZ(0)"
+    },
+    titleBar: {
+      background:
+        "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
+    },
+    tile: {
+      cursor: "pointer"
+    },
+    selectedImage: {
+      marginBottom: "30px",
+      maxWidth: mediaQueryMax320 ? "270px" : "80%",
+      border: "1px solid rgba(0, 0, 0, 0.12)"
+    },
+    selected: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "10px"
+    }
+  }));
   const classes = useStyles();
 
   const tiles = templates.map(tile => {
