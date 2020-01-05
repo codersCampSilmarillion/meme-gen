270f10c2c2c794ca5de622b9a2feab7bcb0ea854
misza <zielinskanoemi5@gmail.com>
2020-01-05 21:34:43 +0100

    Cosmetic changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ImageZoom from 'react-medium-image-zoom'
 
-function meme(props) {
+function Meme(props) {
     let width = props.width * 1.5;
-    let heigh = props.height * 1.5;
+    let height = props.height * 1.5;
+
     return (
         <ImageZoom
             image={{
                 src: props.src,
                 alt: props.name,
-                style: { width: props.width, height: props.height }
+                style: {width: props.width, height: props.height}
             }}
             zoomImage={{
                 src: props.src,
                 alt: props.name,
-                style: { width, height }
+                style: {width, height}
             }}
         />
     )
 }
 
-export default meme;
+export default Meme;
