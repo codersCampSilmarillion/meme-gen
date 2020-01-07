dbcb177f9068a652e48bc3ef1a49054ba28f777a
aronzych <57272508+aronzych@users.noreply.github.com>
2020-01-07 19:18:42 +0100

    Add files via upload

--- src/components/UploadMemeForm.js
+++ src/components/UploadMemeForm.js
@@ -1,16 +1,18 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Popup from "reactjs-popup";
+import Content from "./Content.js";
 
 const objToQuery = obj => {
     const data = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
     return "?" + data.join("&");
-  };
+};
 
-function UploadMemeForm () {
+function UploadMemeForm() {
 
-    const[topText, setTopText] = useState("");
-    const[bottomText, setBottomText] = useState("");
+    const [topText, setTopText] = useState("");
+    const [bottomText, setBottomText] = useState("");
 
     const memeData = {
         template_id: "61579", //na sztywno
@@ -20,6 +22,8 @@ function UploadMemeForm () {
         password: "1silmarillion23"
     };
 
+    let memeUrl;
+
     console.log("top text: " + topText);
     console.log("bottom text: " + bottomText);
 
@@ -27,37 +31,41 @@ function UploadMemeForm () {
         <div>
             <form
                 onSubmit={async e => {
-                e.preventDefault();
-                const response = await fetch(
-                    `https://api.imgflip.com/caption_image${objToQuery(memeData)}`
+                    e.preventDefault();
+                    const response = await fetch(
+                        `https://api.imgflip.com/caption_image${objToQuery(memeData)}`
                     );
                     const json = await response.json();
-                    console.log(json);
+                    memeUrl = (json.data.url);
+                    console.log(memeUrl);
                 }}
-            autoComplete="off"
-            className="search"
+
+
+                autoComplete="off"
+                className="search"
             >
 
-            <TextField
-                label="Top text"
-                variant="outlined"
-                type="text"
-                name = "topText"
-                onChange={e => setTopText(e.target.value)}
-            />
-            <TextField
-                label="Bottom text"
-                variant="outlined"
-                type="text"
-                name = "bottomText"
-                onChange={e => setBottomText(e.target.value)}
-            />
-            <Button variant="contained" type="submit" >Generate meme</Button>
+                <TextField
+                    label="Top text"
+                    variant="outlined"
+                    type="text"
+                    name="topText"
+                    onChange={e => setTopText(e.target.value)}
+                />
+                <TextField
+                    label="Bottom text"
+                    variant="outlined"
+                    type="text"
+                    name="bottomText"
+                    onChange={e => setBottomText(e.target.value)}
+                />
+                <Popup modal trigger={<Button variant="contained" type="submit">Generate Meme</Button>}>
+                    {close => <Content close={close} />}
+                </Popup>
 
             </form>
         </div>
-        );
-          
-      
+    );
 }
+
 export default UploadMemeForm;
