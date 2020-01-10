3b3562955d505d830f44f283545a3ff71f2d6f3e
misza <zielinskanoemi5@gmail.com>
2020-01-10 11:28:37 +0100

    Updates

---
+++ precache-manifest.5c5e88acaf428c26c71989958327d876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1635f7b400c4559fa08058c8a13d7722",
+    "url": "/meme-gen/index.html"
+  },
+  {
+    "revision": "c98dd80d42a18455748d",
+    "url": "/meme-gen/static/css/main.679f548d.chunk.css"
+  },
+  {
+    "revision": "7b640c05963571d04369",
+    "url": "/meme-gen/static/js/2.607ea260.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/meme-gen/static/js/2.607ea260.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c98dd80d42a18455748d",
+    "url": "/meme-gen/static/js/main.c694a5e8.chunk.js"
+  },
+  {
+    "revision": "7d82716a6d3452186bd5",
+    "url": "/meme-gen/static/js/runtime-main.f3fd4f60.js"
+  }
+]);
